1153131a6f5f73b069a2300cc983844cdb5fb3ac
johnniewalker7488 <zenpad7488@gmail.com>
2020-04-22 17:32:46 +0300

    added 1hot

--- Regression/Used_cars_price_prediction/Data_preprocessing.py
+++ Regression/Used_cars_price_prediction/Data_preprocessing.py
@@ -1,3 +1,5 @@
+import warnings
+warnings.filterwarnings("ignore")
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -5,7 +7,8 @@ import matplotlib.pyplot as plt
 from scipy import stats
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, OneHotEncoder
+from sklearn.compose import make_column_transformer
 
 filename = 'Git/ml_projects/Regression/Used_cars_price_prediction/true_car_listings.csv'
 save_path = 'Git/ml_projects/Regression/Used_cars_price_prediction/'
@@ -73,6 +76,7 @@ class Preprocessor:
         print('Histograms for numeric values after preprocessing saved to current directory')
 
         # Use 'US City Populations.csv' to encode the 'City' column
+        '''
         us_cities = pd.read_excel('Git/ml_projects/Regression/Used_cars_price_prediction/US City Populations.xlsx')
         us_cities.drop('State', axis=1, inplace=True)
         us_cities.sort_values('Population', ascending=False)
@@ -85,7 +89,8 @@ class Preprocessor:
         # Encode the city column with 1 for city population more than 50000 and 0 for less 50000
         # and remove the unnecessary columns
         df_clean['City'] = np.where(df_clean['Population'] > 50000, 1, 0)
-        df_clean.drop(['Vin', 'Population'], axis=1, inplace=True)
+        '''
+        df_clean.drop(['City', 'Vin'], axis=1, inplace=True)
 
         # Preprocess the categorical columns
         df_clean['State'] = df_clean['State'].str.lower()
@@ -93,6 +98,10 @@ class Preprocessor:
         df_clean['Model'] = df_clean['Model'].str.lower()
         df_clean['Model'] = df_clean['Model'].str.replace(' ', '')
         df_clean['Model'] = df_clean['Model'].str.replace('-', '')
+
+        # Dropping rare models
+        cars_drop = df_clean['Model'].value_counts()[171:]
+        df_clean.drop(df_clean[df_clean.Model.isin(cars_drop.index)].index, inplace=True)
 
         # Take a particular car to show linearity
         honda = df_clean[(df_clean.Make == 'honda') & (df_clean.Model == 'accord')]
@@ -106,22 +115,24 @@ class Preprocessor:
         sns.scatterplot(x='Year', y='Price', data=honda, marker='x')
         plt.savefig(save_path + 'Year_vs_price.png')
         print('Year vs price plot saved to current directory')
+        print('\nCleaned data shape: ', df_clean.shape)
         print('\nData cleaned\n')
         df_clean.to_csv(save_path + 'Clean_data.csv', encoding='utf-8', index=False)
 
         return df_clean
 
-    def label_encode(self, dataframe):
-        """Encodes categorical data with LabelEncoder"""
+    # def encoder(self, dataframe):
+    #     """Encodes categorical data"""
+        
+    #     df_to_encode = dataframe
+    #     column_trans = make_column_transformer((OneHotEncoder(), ['State', 'Make', 'Model']),
+    #                                             remainder='passthrough')
 
-        encoder = LabelEncoder()
-        df_encoded = dataframe
-        df_encoded['State'] = encoder.fit_transform(df_encoded['State'])
-        df_encoded['Make'] = encoder.fit_transform(df_encoded['Make'])
-        df_encoded['Model'] = encoder.fit_transform(df_encoded['Model'])
-        print('Categorical features encoded')
+    #     df_encoded = column_trans.fit_transform(df_to_encode)
+    #     #df_joint = pd.concat([df_numeric, df_encoded], axis=1)
+    #     print('Categorical features encoded: ', df_encoded.shape)
 
-        return df_encoded
+    #     return df_encoded
     
     def split_data(self, dataframe):
         """Splits the data into train and test"""
@@ -129,16 +140,22 @@ class Preprocessor:
         X = dataframe.drop('Price', axis=1)
         y = dataframe['Price']
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        column_trans = make_column_transformer((OneHotEncoder(), ['State', 'Make', 'Model']),
+                                                remainder='passthrough')
+        X = column_trans.fit_transform(X)
 
-        print('Data split into train and test sets')
+        print('\nShape of encoded training data: ', X.shape)
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=1)
+
+        print('\nData split into train and test sets')
 
         return X_train, X_test, y_train, y_test
     
     def scale_numeric(self, X_train, X_test):
         """Scales numeric data"""
 
-        scaler = MinMaxScaler()
+        scaler = StandardScaler()
         X_train.iloc[:, 0:2] = scaler.fit_transform(X_train.iloc[:, 0:2])
         X_test.iloc[:, 0:2] = scaler.transform(X_test.iloc[:, 0:2])
 
@@ -149,11 +166,13 @@ class Preprocessor:
     def run_preprocessing(self):
         df_raw = self.import_data(filename)
         df_clean = self.clean_data(df_raw)
-        df_encoded = self.label_encode(df_clean)
-        X_train, X_test, y_train, y_test = self.split_data(df_encoded)
-        X_train, X_test = self.scale_numeric(X_train, X_test)
+        #df_encoded = self.encoder(df_clean)
+        X_train, X_test, y_train, y_test = self.split_data(df_clean)
+        #X_train_sc, X_test_sc = self.scale_numeric(X_train, X_test)
 
-        print('\nData has been preprocessed for training')
+        print('\nData has been preprocessed for training\n')
+
+        return X_train, X_test, y_train, y_test
 
 
 if __name__ == "__main__":
